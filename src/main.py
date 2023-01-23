f76753229df0b099fba334c8e224538583be8a03
EC2 Default User <ec2-user@ip-172-31-32-186.us-west-2.compute.internal>
2023-01-23 19:50:56 +0000

    fix main file

--- src/main.py
+++ src/main.py
@@ -12,6 +12,11 @@ files = s3.list_files(prefix=file_prefix, checkpoint=checkpoint)
 
 print(files)
 
+a = redshift.db_cursor.execute("CREATE TABLE ebdb_dynamo_user_sessions (id integer primary key, partition_key character varying, primary_sort_key character varying, auth_method character varying, device_properties_client_name character varying, device_properties_client_version character varying, device_properties_device_type character varying, device_properties_os_name character varying, device_properties_os_version character varying, location_city character varying, location_country character varying, location_country_code character varying, location_ip character varying, location_postal_code character varying, location_region character varying, location_region_code character varying, product character varying, remote_ip character varying, session_state character varying, user_agent character varying, created_at timestamp, session_expiry_time timestamp, updated_at timestamp) compound sortkey (partition_key, primary_sort_key);")
+
+print(a)
+print("hello")
+
 # Loading env variables -> `export $(xargs <.env)`
 
 insert_ops = []
