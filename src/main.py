1bd1f5822a853f00694dd1e691e6eb1128d94307
Shubham Anand <shubham@interviewbit.com>
2023-01-24 03:07:03 +0530

    update execution logic

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import json
 import redshift
 import s3
+import time
 from datetime import datetime
 
 file_prefix = datetime.now().strftime("%Y/%m/%d")
@@ -14,19 +15,15 @@ print(files)
 
 # Loading env variables -> `export $(xargs <.env)`
 
-insert_ops = []
-remove_ops = []
-
 for file_key in files:
   file_content = []
   for line in s3.file_iterator(file_key):
     file_content += json.loads(line.decode())
 
-  insert_ops += list(filter(lambda item: list(item.keys())[0] == 'INSERT' and item['INSERT']['keys']['partition_key'].startswith("scaler"), file_content))
-  remove_ops += list(filter(lambda item: list(item.keys())[0] == 'REMOVE' and item['REMOVE']['keys']['partition_key'].startswith("scaler"), file_content))
-#  s3.create_checkpoint(file_key)
+  insert_ops = list(filter(lambda item: list(item.keys())[0] == 'INSERT' and item['INSERT']['keys']['partition_key'].startswith("scaler"), file_content))
+  remove_ops = list(filter(lambda item: list(item.keys())[0] == 'REMOVE' and item['REMOVE']['keys']['partition_key'].startswith("scaler"), file_content))
 
-print('insert')
-redshift.batch_insert(insert_ops)
-print('delete', remove_ops)
-redshift.batch_remove(remove_ops)
+  redshift.batch_insert(insert_ops)
+  redshift.batch_remove(remove_ops)
+
+  s3.create_checkpoint(file_key)
